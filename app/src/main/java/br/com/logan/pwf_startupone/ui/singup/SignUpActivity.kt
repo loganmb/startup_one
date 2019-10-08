76f9899962d80c03f4d781d7bf6f52d31bc073d3
luizkt <keese.luiz@gmail.com>
2019-10-08 20:26:08 -0300

    Login via Firebase

--- app/src/main/java/br/com/logan/pwf_startupone/ui/singup/SignUpActivity.kt
+++ app/src/main/java/br/com/logan/pwf_startupone/ui/singup/SignUpActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.logan.pwf_startupone.R
 
-class SingUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
